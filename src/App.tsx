05d3ba271511cc4034e201fb3506bf605f79347b
Jhacss <henryki09.joao@gmail.com>
2025-02-28 14:43:03 -0300

    :sparkles: Implementação Cadastrar Veiculo e atualizar Veiculo

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import ListaViagens from "./components/viagem/listaviagem/ListaViagem";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import ListaVeiculos from "./components/veiculos/listaveiculos/ListaVeiculos";
+import FormVeiculo from "./components/veiculos/veiculocadastro/FormVeiculo";
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
           <Route path="/editarviagem/:id" element={<FormViagem />} />
           <Route path="/viagens" element={<ListaViagens />} />
           <Route path="/veiculos" element={<ListaVeiculos />} />
-
+          <Route path="/cadastrarveiculo" element={<FormVeiculo />} />
+          <Route path="/atualizarveiculo/:id" element={<FormVeiculo />} />
         </Routes>
         <Footer />
       </BrowserRouter>
