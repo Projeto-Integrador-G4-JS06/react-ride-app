fe127b3729d06f790af5239d9824ca1a00ed59ee
Gabriela Teixeira <gaba.andrade@outlook.com>
2025-02-28 12:25:28 -0300

    :sparkles: Inserir CardVeiculos na ListaVeiculos

--- src/components/veiculos/cardveiculo/CardVeiculo.tsx
+++ src/components/veiculos/cardveiculo/CardVeiculo.tsx
@@ -10,11 +10,14 @@ function CardVeiculo({ veiculo }: CardVeiculoProps) {
     <div className="flex flex-col justify-between my-10 mx-10 overflow-hidden bg-gray-50 w-[13.5rem] h-[20rem] rounded-lg">
       <div className="flex items-end  border-amber-500 bg-orange py-1.5 justify-end pt-2 pr-2">
         {/* <Link to={`/editarveiculo/${veiculo.id}`}> */}
-        <Pencil size={24} className="mr-1 hover:fill-teal-800" />
+        <Pencil
+          size={24}
+          color="#293241"
+          className="mr-1 hover:fill-teal-800"
+        />
         {/* </Link> */}
-
         {/* <Link to={`/deletarveiculo/${veiculo.id}`}> */}
-        <Trash size={24} className="mr-1 hover:fill-red-700" />
+        <Trash size={24} color="#293241" className="mr-1 hover:fill-red-700" />
         {/* </Link> */}
       </div>
 
@@ -26,11 +29,11 @@ function CardVeiculo({ veiculo }: CardVeiculoProps) {
         />
 
         <div className="p-4 flex flex-col items-center">
-          <p className="text-sm font-poppins">Marca:{veiculo.marca} </p>
-          <p className="text-sm font-poppins">Modelo:{veiculo.modelo}</p>
-          <p className="text-sm font-poppins">Ano:{veiculo.ano}</p>
-          <p className="text-sm font-poppins">Cor:{veiculo.cor}</p>
-          <p className="text-sm font-poppins">Placa:{veiculo.placa}</p>
+          <p className="text-sm font-poppins">Marca: {veiculo.marca} </p>
+          <p className="text-sm font-poppins">Modelo: {veiculo.modelo}</p>
+          <p className="text-sm font-poppins">Ano: {veiculo.ano}</p>
+          <p className="text-sm font-poppins">Cor: {veiculo.cor}</p>
+          <p className="text-sm font-poppins">Placa: {veiculo.placa}</p>
         </div>
       </div>
     </div>
