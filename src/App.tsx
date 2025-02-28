743b2db170e915d73807f346dc0cdef6eedc765e
Samuel <samuka2568@gmail.com>
2025-02-28 15:08:10 -0300

    :sparkles: Criar formularios

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import FormViagem from "./components/viagem/viagemcadastro/FormViagem";
 import ListaViagens from "./components/viagem/listaviagem/ListaViagem";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/cadastrarviagem" element={<FormViagem />} />
           <Route path="/editarviagem/:id" element={<FormViagem />} />
           <Route path="/viagens" element={<ListaViagens />} />
